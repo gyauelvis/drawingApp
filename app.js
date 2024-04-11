96d980cb7a931d5a3eb81613d57df9f72a09a621
gyauelvis <gyauelvis@gmail.com>
2024-04-11 10:26:44 +0000

    cursor functionality implemented

--- app.js
+++ app.js
@@ -1,119 +1,137 @@
 let CanvasApp = {
-    canvas: document.querySelector('#canvas'),
-    isDrawn: false,
-    colorMenuStatus: false,
-    brushMenuStatus: false,
-    brushColor: "black",
-    lineWidth: 2,
-    isErase: false,
-    coverWholeScreen(){
-        CanvasApp.canvas.height = window.innerHeight;
-        CanvasApp.canvas.width = window.innerWidth;
-    },
-    draw(){
-        CanvasApp.isDrawn = true;
-        CanvasApp.canvas.getContext("2d").beginPath();
-    },
-    stopDraw(){
-        CanvasApp.isDrawn = false;
-    },
-    letDraw(e){
-        if(CanvasApp.isDrawn){
-            const ctx = CanvasApp.canvas.getContext("2d");
-            ctx.lineWidth = CanvasApp.lineWidth;
-            ctx.lineCap = "round";
-            ctx.lineTo(e.clientX, e.clientY);
-            ctx.stroke();
-            ctx.moveTo(e.clientX, e.clientY);
-        }
-        
-    },
-    colorMenu(){
-        if(!CanvasApp.colorMenuStatus){
-            document.getElementById("brushes").style.display = "none";
-            CanvasApp.brushMenuStatus = false;
-            document.getElementById("colors").style.display = "flex";
-            CanvasApp.colorMenuStatus = true;
-        }else{
-            document.getElementById("colors").style.display = "none";
-            CanvasApp.colorMenuStatus = false;
-        }
-    },
+  canvas: document.querySelector("#canvas"),
+  isDrawn: false,
+  colorMenuStatus: false,
+  brushMenuStatus: false,
+  brushColor: "black",
+  lineWidth: 2,
+  isErase: false,
 
-    brushMenu(e){
-        e.preventDefault();
-        if(!CanvasApp.brushMenuStatus){
-            document.getElementById("colors").style.display = "none";
-            CanvasApp.colorMenuStatus = false;
-            document.getElementById("brushes").style.display = "flex";
-            CanvasApp.brushMenuStatus = true;
-            document.querySelectorAll(".brush").forEach(elem =>elem.style.backgroundColor = CanvasApp.brushColor);
-        }else{
-            document.getElementById("brushes").style.display = "none";
-            CanvasApp.brushMenuStatus = false;
-        }
-    },
-
-    selectedColor(color){
-        CanvasApp.canvas.getContext("2d").strokeStyle = color;
-        CanvasApp.brushColor = color;
-    },
-
-    eraserLine(e){
-        e.preventDefault();
-        if(CanvasApp.isErase){
-            const ctx = CanvasApp.canvas.getContext("2d");
-            ctx.clearRect(e.clientX, e.clientY, 7,7)
-            ctx.moveTo(e.clientX,e.clientY)
-        }
-        
+  coverWholeScreen() {
+    CanvasApp.canvas.height = window.innerHeight;
+    CanvasApp.canvas.width = window.innerWidth;
+  },
+  draw() {
+    CanvasApp.isDrawn = true;
+    CanvasApp.canvas.getContext("2d").beginPath();
+  },
+  stopDraw() {
+    CanvasApp.isDrawn = false;
+  },
+  letDraw(e) {
+    if (CanvasApp.isDrawn) {
+      const ctx = CanvasApp.canvas.getContext("2d");
+      ctx.lineWidth = CanvasApp.lineWidth;
+      ctx.lineCap = "round";
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
+      ctx.moveTo(e.clientX, e.clientY);
     }
-}
+  },
+  colorMenu() {
+    if (!CanvasApp.colorMenuStatus) {
+      document.getElementById("brushes").style.display = "none";
+      CanvasApp.brushMenuStatus = false;
+      document.getElementById("colors").style.display = "flex";
+      CanvasApp.colorMenuStatus = true;
+    } else {
+      document.getElementById("colors").style.display = "none";
+      CanvasApp.colorMenuStatus = false;
+    }
+  },
 
-window.addEventListener('load',()=>{
-    CanvasApp.coverWholeScreen();
-    document.querySelectorAll(".brush").forEach(elem =>elem.style.backgroundColor = CanvasApp.brushColor)
+  brushMenu(e) {
+    e.preventDefault();
+    if (!CanvasApp.brushMenuStatus) {
+      document.getElementById("colors").style.display = "none";
+      CanvasApp.colorMenuStatus = false;
+      document.getElementById("brushes").style.display = "flex";
+      CanvasApp.brushMenuStatus = true;
+      document
+        .querySelectorAll(".brush")
+        .forEach((elem) => (elem.style.backgroundColor = CanvasApp.brushColor));
+    } else {
+      document.getElementById("brushes").style.display = "none";
+      CanvasApp.brushMenuStatus = false;
+    }
+  },
+
+  selectedColor(color) {
+    CanvasApp.canvas.getContext("2d").strokeStyle = color;
+    CanvasApp.brushColor = color;
+  },
+
+  eraserLine(e) {
+    e.preventDefault();
+    if (CanvasApp.isErase) {
+      const ctx = CanvasApp.canvas.getContext("2d");
+      ctx.clearRect(e.clientX, e.clientY, 7, 7);
+      ctx.moveTo(e.clientX, e.clientY);
+    }
+  },
+};
+let cursor = document.querySelector(".cursor");
+window.addEventListener("load", (e) => {
+  CanvasApp.coverWholeScreen();
+  document
+    .querySelectorAll(".brush")
+    .forEach((elem) => (elem.style.backgroundColor = CanvasApp.brushColor));
 });
 
-CanvasApp.canvas.addEventListener('mousedown',CanvasApp.draw);
-CanvasApp.canvas.addEventListener('mouseup',CanvasApp.stopDraw);
-CanvasApp.canvas.addEventListener('mousemove',CanvasApp.letDraw);
-window.addEventListener('load',CanvasApp.letDraw);
-window.addEventListener('resize', CanvasApp.coverWholeScreen);
-document.querySelector('.palette').addEventListener('click',CanvasApp.colorMenu);
-document.querySelector('.brush-btn').addEventListener('click',CanvasApp.brushMenu)
+CanvasApp.canvas.addEventListener('mousemove', (e) => {
+    cursor.style.top =( e.pageY) + 'px';
+    cursor.style.left = (e.pageX) + 'px';
+    if(e.pageY  <= 5)cursor.style.display = 'none'
+    else cursor.style.display = 'block'
+});
 
-document.querySelectorAll('.color').forEach(elem =>{
-    elem.addEventListener('click',(e)=>{
-        e.preventDefault();
-        CanvasApp.brushColor = e.target.title;
-        CanvasApp.selectedColor( e.target.title);
-        document.getElementById("colors").style.display = "none";
-        CanvasApp.colorMenuStatus = false;
-    })
+CanvasApp.canvas.addEventListener("mousedown", CanvasApp.draw);
+CanvasApp.canvas.addEventListener("mouseup", CanvasApp.stopDraw);
+CanvasApp.canvas.addEventListener("mousemove", CanvasApp.letDraw);
+window.addEventListener("load", CanvasApp.letDraw);
+window.addEventListener("resize", CanvasApp.coverWholeScreen);
+document
+  .querySelector(".palette")
+  .addEventListener("click", CanvasApp.colorMenu);
+document
+  .querySelector(".brush-btn")
+  .addEventListener("click", CanvasApp.brushMenu);
+
+document.querySelectorAll(".color").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    e.preventDefault();
+    CanvasApp.brushColor = e.target.title;
+    document.querySelector('.cursor').style.backgroundColor = e.target.title;
+    CanvasApp.selectedColor(e.target.title);
+    document.getElementById("colors").style.display = "none";
+    CanvasApp.colorMenuStatus = false;
+  });
+});
+
+document.querySelector('#menu').addEventListener('mouseenter', (e)=>{
+  e.preventDefault();
+  cursor.style.display = "none";
 })
 
+document.querySelectorAll(".brush").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    e.preventDefault();
+    CanvasApp.lineWidth = Number(elem.classList[1].slice(4));
+    document.querySelector('.cursor').style.width = `${Number(elem.classList[1].slice(4))*2}px`;
+    document.querySelector('.cursor').style.height = `${Number(elem.classList[1].slice(4))*2}px`;
+    document.getElementById("brushes").style.display = "none";
+    CanvasApp.brushMenuStatus = false;
+  });
+});
 
-document.querySelectorAll(".brush").forEach(elem =>{
-    elem.addEventListener("click",(e)=>{
-        e.preventDefault();
-        CanvasApp.lineWidth =  Number(elem.classList[1].slice(4));
-        document.getElementById("brushes").style.display = "none";
-        CanvasApp.brushMenuStatus = false;
-    });
-})
-
-
-
-document.querySelector(".eraser").addEventListener("click", (e)=>{
-    if(CanvasApp.isErase){
-        CanvasApp.canvas.removeEventListener('mousemove',CanvasApp.eraserLine);
-        CanvasApp.canvas.addEventListener('mousemove',CanvasApp.letDraw);
-        CanvasApp.isErase = false;
-    }
-    else{
-        CanvasApp.canvas.removeEventListener('mousemove',CanvasApp.letDraw);
-        CanvasApp.canvas.addEventListener('mousemove',CanvasApp.eraserLine);
-        CanvasApp.isErase = true;
-    }
+document.querySelector(".eraser").addEventListener("click", (e) => {
+  if (CanvasApp.isErase) {
+    CanvasApp.canvas.removeEventListener("mousemove", CanvasApp.eraserLine);
+    CanvasApp.canvas.addEventListener("mousemove", CanvasApp.letDraw);
+    CanvasApp.isErase = false;
+  } else {
+    CanvasApp.canvas.removeEventListener("mousemove", CanvasApp.letDraw);
+    CanvasApp.canvas.addEventListener("mousemove", CanvasApp.eraserLine);
+    CanvasApp.isErase = true;
+  }
 });
